58262085211aeb466f8e56f8dbd0a607b6fb5e34
mufeed <mufeeddaralmanar123@gmail.com>
2025-03-24 12:30:22 +0530

    resizing pic

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 
 import { EarthCanvas } from "../canvas";
 import { SectionWrapper } from "../../hoc";
@@ -16,12 +15,6 @@ const INITIAL_STATE = Object.fromEntries(
   Object.keys(config.contact.form).map((input) => [input, ""])
 );
 
-const emailjsConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  accessToken: import.meta.env.VITE_EMAILJS_ACCESS_TOKEN,
-};
-
 const Contact = () => {
   const formRef = useRef<React.LegacyRef<HTMLFormElement> | undefined>();
   const [form, setForm] = useState(INITIAL_STATE);
@@ -35,39 +28,6 @@ const Contact = () => {
     setForm({ ...form, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement> | undefined) => {
-    if (e === undefined) return;
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs
-      .send(
-        emailjsConfig.serviceId,
-        emailjsConfig.templateId,
-        {
-          name: form.name,
-          to_name: config.html.fullName,
-          email: form.email,
-          to_email: config.html.email,
-          message: form.message,
-        },
-        emailjsConfig.accessToken
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm(INITIAL_STATE);
-        },
-        (error) => {
-          setLoading(false);
-
-          console.log("EmailJS error:", JSON.stringify(error, null, 2));
-          alert("Something went wrong.");
-        }
-      );
-  };
 
   return (
     <div
@@ -77,15 +37,17 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className="bg-black-100 flex-[0.75] rounded-2xl p-8"
       >
-        
-      
+
+
         <Header useMotion={false} {...config.contact} />
 
         <form
           // @ts-expect-error
           ref={formRef}
-          onSubmit={handleSubmit}
           className="mt-12 flex flex-col gap-8"
+          action="/"
+          method="POST"
+          netlify // Add netlify attribute for Netlify Forms
         >
           {Object.keys(config.contact.form).map((input) => {
             const { span, placeholder } =
