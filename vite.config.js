68832084ebc08603f851473058258366c77b5eff
mufeed <mufeeddaralmanar123@gmail.com>
2025-03-25 10:56:36 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",  // Ensures correct asset paths in production
   plugins: [react()],
- build: {
-   rollupOptions: {
-     output: {
-       manualChunks(id) {
-         if (id.includes('node_modules')) {
-           if (id.includes('three') || id.includes('three-stdlib') || id.includes('@emailjs/browser')) {
-             return 'vendor-three';
-           }
-           return 'vendor';
-         }
-       },
-     },
-   },
- },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("three") || id.includes("three-stdlib") || id.includes("@emailjs/browser")) {
+              return "vendor-three";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  define: {
+    "process.env": {}, // Prevents SSR issues
+  },
 });
